47ec5834924f97012c6734bc793a36d8218b68d4
Erik Oosting <crazazy@tilde.cafe>
2023-06-01 14:52:09 +0200

    add DyNetKAT types

--- src/Data/NetKat.hs
+++ src/Data/NetKat.hs
@@ -2,7 +2,85 @@ module Data.NetKat where
 
 import Control.Applicative (Alternative (..))
 import Data.List (iterate, transpose)
+import Data.Word (Word8 (..))
 
+data Packet = Packet
+    { src :: Word8
+    , dest :: Word8
+    , port :: Word8
+    , typ :: Word8
+    }
+    deriving (Show, Read, Eq, Ord)
+data Filter
+    = Src
+    | Dest
+    | Port
+    | Typ
+    deriving (Show, Read, Eq)
+
+toBinary :: Word8 -> String
+toBinary x = go 8 x
+  where
+    go 0 _ = []
+    go y x = (if x `mod` 2 == 1 then '1' else '0') : (go (y - 1) $ div x 2)
+
+-- turns a packet into a binary vector
+-- >>> pToBV (Packet 4 2 23 1)
+pToBV :: Packet -> String
+pToBV (Packet src dest port typ) =
+    toBinary src
+        ++ toBinary dest
+        ++ toBinary port
+        ++ toBinary typ
+  where
+
+filterToBV :: Filter -> Word8 -> String
+filterToBV Src x = toBinary x ++ replicate 24 '?'
+filterToBV Dest x = replicate 8 '?' ++ toBinary x ++ replicate 16 '?'
+filterToBV Port x = replicate 16 '?' ++ toBinary x ++ replicate 8 '?'
+filterToBV Typ x = replicate 24 '?' ++ toBinary x
+
+data NetKat
+    = Nil
+    | Pass
+    | Test Filter Word8
+    | Not NetKat
+    | Change Filter Word8
+    | Or NetKat NetKat
+    | Seq NetKat NetKat
+    | Closure NetKat
+    deriving (Eq, Show, Read)
+parens :: String -> String
+parents s = "(" ++ s ++ ")"
+toKatBV :: NetKat -> String
+toKatBV Nil = "F"
+toKatBV Pass = "T"
+toKatBV (Test f w) = parens $ "x=" ++ filterToBV f w
+toKatBV (Not p) = parens $ '~' : (toKatBV p)
+toKatBV (Change f w) = parens $ "x:=" ++ filterToBV f w
+toKatBV (Or p1 p2) = parens $ (toKatBV p1) ++ "+" ++ (toKatBV p2)
+toKatBV (Seq p1 p2) = parens $ (toKatBV p1) ++ ";" ++ (toKatBV p2)
+toKatBV (Closure p) = parens $ "(" ++ toKatBV p ++ ")*"
+
+data DyNetKat
+    = Prog NetKat DyNetKat
+    | Ident String
+    | Ask String NetKat
+    | Send String NetKat
+    | Disj DyNetKat DyNetKat
+    | Par DyNetKat DyNetKat
+    deriving (Eq, Show, Read)
+
+data Transistion
+    = Chg Packet [Packet]
+    | TAsk String NetKat
+    | TSend String NetKat
+    | Rcfg String NetKat
+
+step :: [(String, DyNetKat)] -> DyNetKat -> [DyNetKat]
+step = undefined
+
+{-
 data NetKat a
     = Nil
     | Pass
@@ -25,3 +103,4 @@ interpret (Closure p) a = concat $ takeWhile (not . null) $ (map (flip interpret
 
 flowtable :: Eq a => [(a, a)] -> NetKat a
 flowtable = Or . map (\(a, b) -> Seq (Test (a ==)) (Change (const b)))
+-}
