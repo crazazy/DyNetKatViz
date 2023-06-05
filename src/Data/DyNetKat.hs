8bf99dbd8e844afed451245507b370939496c082
Erik Oosting <crazazy@tilde.cafe>
2023-06-05 16:40:29 +0200

    new temp solution

--- src/Data/DyNetKat.hs
+++ src/Data/DyNetKat.hs
@@ -29,8 +29,8 @@ data Transition
 
 -- changes 1 layer of identifiers to their respective DyNetKat program
 transformIdent :: [(String, DyNetKat)] -> DyNetKat -> DyNetKat
+transformIdent _ Bottom = Bottom
 transformIdent env (Ident s) = maybe Bottom id $ lookup s env
-transformIdent env Bottom = Bottom
 transformIdent env (Prog nk nx) = Prog nk $ transformIdent env nx
 transformIdent env (Ask s nk nx) = Ask s nk $ transformIdent env nx
 transformIdent env (Send s nk nx) = Send s nk $ transformIdent env nx
@@ -38,19 +38,21 @@ transformIdent env (Disj n1 n2) = Disj (transformIdent env n1) (transformIdent e
 transformIdent env (Par n1 n2) = Par (transformIdent env n1) (transformIdent env n2)
 
 -- | Performs a step in a DyNetKat program with all definitions loaded
-step :: [(String, DyNetKat)] -> Packet -> DyNetKat -> [(Transition, DyNetKat)]
-step env pa pr = case pr of
+step :: [(String, DyNetKat)] -> [Packet] -> DyNetKat -> [(Transition, DyNetKat)]
+step env pas pr = case pr of
     Bottom -> [] -- end of program
-    Ident s -> (maybe [] (: []) $ lookup s env) >>= step env pa -- rule 2
-    Prog nk nx -> [(Chg pa (mockEval nk pa), nx)] -- rule 1
+    Ident s -> (maybe [] (: []) $ lookup s env) >>= step env pas -- rule 2
+    Prog nk nx -> case pas of
+        [] -> []
+        (pa : _) -> [(Chg pa (mockEval nk pa), nx)] -- rule 1
     Ask s nk nx -> [(TAsk s nk, nx)] -- rule 7
     Send s nk nx -> [(TSend s nk, nx)] -- rule 8
-    Disj n1 n2 -> (step env pa n1) ++ (step env pa n2) -- rule 3 + 4
+    Disj n1 n2 -> (step env pas n1) ++ (step env pas n2) -- rule 3 + 4
     -- BUG: program doesn't see through Ident's fully yet
     Par n1 n2 ->
         -- rule 5 + 6 + 9 + 10
-        (map (fmap (\x -> Par x n2)) $ step env pa n1)
-            ++ (map (fmap (\x -> Par n1 x)) $ step env pa n2)
+        (map (fmap (\x -> Par x n2)) $ step env pas n1)
+            ++ (map (fmap (\x -> Par n1 x)) $ step env pas n2)
             ++ checkRcfg (transformIdent env n1) (transformIdent env n2)
 
 -- | Basically rule 9 & 10 from the paper (with symmetry)
@@ -60,27 +62,27 @@ checkRcfg (Send s1 nk1 nx1) (Ask s2 nk2 nx2) | s1 == s2 && nk1 == nk2 = [(Rcfg s
 checkRcfg _ _ = [] -- we don't need to take action on the rest since that is handled by `step`
 
 -- | transform a set of definitions and a DyNetKat program into a set of program transition triplets
-eval :: [(String, DyNetKat)] -> DyNetKat -> [Packet] -> [(DyNetKat, Transition, DyNetKat)]
-eval env entry packets = Set.toList $ go env [entry] packets Set.empty
+eval :: [(String, DyNetKat)] -> DyNetKat -> [Packet] -> [(DyNetKat, Transition, DyNetKat, [Packet], [Packet])]
+eval env entry [] = do
+    (trans, new) <- step env [] entry
+    return (entry, trans, new, [], [])
+eval env entry (p : packets) = Set.toList $ go Set.empty (mkHistory $ step env packets entry)
   where
-    go env entries [] transitions = transitions
-    go env entries (p : packets) transitions =
+    mkHistory transitions = do
+        (trans, new) <- transitions
+        return $ if isChg trans then (entry, trans, new, packets, [p]) else (entry, trans, new, (p : packets), [])
+    -- go returns a set of tuples of the form (old program, transition, new program, future packets, past packets)
+    go :: Set.Set (DyNetKat, Transition, DyNetKat, [Packet], [Packet]) -> [(DyNetKat, Transition, DyNetKat, [Packet], [Packet])] -> Set.Set (DyNetKat, Transition, DyNetKat, [Packet], [Packet])
+    go transitions [] = transitions
+    go transitions newTransitions =
         let
-            newOutputs = do
-                entry <- transformIdent env <$> entries
-                (transition, next) <- step env p entry
-                return (entry, transition, next)
-            hasChanges = any (\(_, t, _) -> isChg t) newOutputs
-            newTransitions = Set.fromList $ newOutputs
-            newPrograms = Set.fromList $ (\(_, _, p) -> p) <$> newOutputs
-            existingPrograms = Set.map (\(p, _, _) -> p) transitions
-            unexploredPrograms = Set.difference newPrograms existingPrograms
-            fullyExplored = null newTransitions
-            nextPackets = if hasChanges then packets else p : packets
+            nextTransitions = do
+                (_, _, new, future, history) <- newTransitions
+                (trans', new') <- step env future new
+                return (new, trans', new', if isChg trans' then tail future else future, if isChg trans' then (head future) : history else history)
          in
-            if fullyExplored
-                then transitions
-                else go env (Set.toList unexploredPrograms) nextPackets $ Set.union transitions newTransitions
+            go (Set.union transitions $ Set.fromList newTransitions) $
+                filter (\out -> not $ Set.member out transitions) nextTransitions
 
 isChg :: Transition -> Bool
 isChg (Chg _ _) = True
