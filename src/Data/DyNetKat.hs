89b3fa8d705d387b1366ce83836f93e21d72be24
Erik Oosting <crazazy@tilde.cafe>
2023-06-05 16:59:43 +0200

    renaming Disj
    renaming from Disj (of "Disjunction") to NonDet (of "Non-Deterministic Choice")

--- src/Data/DyNetKat.hs
+++ src/Data/DyNetKat.hs
@@ -15,7 +15,7 @@ data DyNetKat
     | Ident String
     | Ask String NetKat DyNetKat
     | Send String NetKat DyNetKat
-    | Disj DyNetKat DyNetKat
+    | NonDet DyNetKat DyNetKat
     | Par DyNetKat DyNetKat
     | Bottom
     deriving (Eq, Show, Read, Ord)
@@ -34,7 +34,7 @@ transformIdent env (Ident s) = maybe Bottom id $ lookup s env
 transformIdent env (Prog nk nx) = Prog nk $ transformIdent env nx
 transformIdent env (Ask s nk nx) = Ask s nk $ transformIdent env nx
 transformIdent env (Send s nk nx) = Send s nk $ transformIdent env nx
-transformIdent env (Disj n1 n2) = Disj (transformIdent env n1) (transformIdent env n2)
+transformIdent env (NonDet n1 n2) = NonDet (transformIdent env n1) (transformIdent env n2)
 transformIdent env (Par n1 n2) = Par (transformIdent env n1) (transformIdent env n2)
 
 -- | Performs a step in a DyNetKat program with all definitions loaded
@@ -47,7 +47,7 @@ step env pas pr = case pr of
         (pa : _) -> [(Chg pa (mockEval nk pa), nx)] -- rule 1
     Ask s nk nx -> [(TAsk s nk, nx)] -- rule 7
     Send s nk nx -> [(TSend s nk, nx)] -- rule 8
-    Disj n1 n2 -> (step env pas n1) ++ (step env pas n2) -- rule 3 + 4
+    NonDet n1 n2 -> (step env pas n1) ++ (step env pas n2) -- rule 3 + 4
     -- BUG: program doesn't see through Ident's fully yet
     Par n1 n2 ->
         -- rule 5 + 6 + 9 + 10
@@ -92,17 +92,17 @@ isChg _ = False
 example :: [(String, DyNetKat)]
 example =
     [ ("Init", Par (Ident "Host") (Ident "Switch"))
-    , ("Host", Disj (Send "secConReq" Pass $ Ident "Host") (Send "secConEnd" Pass $ Ident "Host"))
+    , ("Host", NonDet (Send "secConReq" Pass $ Ident "Host") (Send "secConEnd" Pass $ Ident "Host"))
     ,
         ( "Switch"
-        , Disj (Prog ((Test Port 0) `Seq` (Change Port 1)) $ Ident "Switch") $
-            Disj (Prog ((Test Port 1) `Seq` Nil) $ Ident "Switch") $
+        , NonDet (Prog ((Test Port 0) `Seq` (Change Port 1)) $ Ident "Switch") $
+            NonDet (Prog ((Test Port 1) `Seq` Nil) $ Ident "Switch") $
                 (Ask "secConReq" Pass $ Ident "Switch'")
         )
     ,
         ( "Switch'"
-        , Disj (Prog ((Test Port 0) `Seq` (Change Port 1)) $ Ident "Switch'") $
-            Disj (Prog ((Test Port 1) `Seq` (Change Port 0)) $ Ident "Switch'") $
+        , NonDet (Prog ((Test Port 0) `Seq` (Change Port 1)) $ Ident "Switch'") $
+            NonDet (Prog ((Test Port 1) `Seq` (Change Port 0)) $ Ident "Switch'") $
                 (Ask "secConEnd" Pass $ Ident "Switch")
         )
     ]
